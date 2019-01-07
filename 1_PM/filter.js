fa796ae35c057c910ccc7ab756bba695ec2ff5b7
Lara <larataneeb@college.harvard.edu>
2019-01-07 17:02:59 -0500

    1 PM solutions

--- 1_PM/filter.js
+++ 1_PM/filter.js
@@ -4,11 +4,25 @@
 const arr = [7, 8, 15, 14, 13, 70, 44, 35, 88, 77, false, "7", "hi", null];
 
 const multOf7 = el => {
-  // TODO
+  if (typeof el !== "number") {
+    return false
+  }
+  else {
+    const remainder = el % 7
+    return remainder == 0  
+  }
 };
 
 Array.prototype.filter = function(fun) {
   // TODO
+  const filtered = []; 
+  for(let i = 0; i < this.length; i++) {
+    if (fun(this[i])){
+       filtered.push(this[i]);
+    }
+  }
+  return filtered;
+  
 };
 
 const newArr = arr.filter(multOf7);
--- 1_PM/map.js
+++ 1_PM/map.js
@@ -5,10 +5,22 @@ const arr = [0, 1, 2, 3, 4];
 
 const triple = () => {
   // TODO
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] * 3;
+  }
 };
 
+
 Array.prototype.map = function(fun) {
-  // TODO
+  // TODO'
+  const mapped  = []
+  for(let i = 0; i < this.length; i++) {
+    if (fun(this[i])){
+       mapped.push(this[i]);
+    }
+  }
+  return mapped;
+
 };
 
 console.log(arr.map(triple));
